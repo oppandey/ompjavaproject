a2b48a6df2ed37f3a0bbd8625a987641eb4bbe03
oppandey <omspandey@hotmail.com>
2019-11-02 05:08:06 -0700

    File committed for LTI Project

---
+++ HelloWorldProj/src/helloworldpack/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloworldpack;
+
+/**
+ * @author Omprakash
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git");
+
+	}
+
+}
